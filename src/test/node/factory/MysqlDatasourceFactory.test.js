557c47e567b1ebf42461aa909bc135a56be8041c
Giani Segatto <gsegatto@travix.com>
2019-05-18 22:58:18 +0100

    tests and configs

---
+++ src/test/node/factory/MysqlDatasourceFactory.test.js
@@ -0,0 +1,28 @@
+const sinon = require("sinon");
+const expect = require("chai").expect;
+
+const mockedMysql = sinon.mock(require("mysql"));
+const MysqlDatasourceFactory = require("../../../main/node/factory/MysqlDatasourceFactory");
+
+describe("MysqlDatasourceFactory", () => {
+
+    it("should create MysqlDatasource", () => {
+        
+        const host = "host";
+        const user = "user";
+        const pass = "pass";
+        const connectionLimit = 1;
+        const database = "database";
+
+        const configuration = { host: host, user: user, pass: pass, database: database, connectionLimit: connectionLimit };
+        const connectionPool = {};
+
+        mockedMysql.expects("createPool").withExactArgs(configuration).returns(connectionPool);
+
+        const mysqlDatasourceFactory = new MysqlDatasourceFactory(host, user, pass, database, connectionLimit);
+
+        const mysqlDatasource = mysqlDatasourceFactory.create();
+
+        expect(mysqlDatasource).to.not.be.null;
+    });
+});
